9efca0ba733755cf55ec4d8328585865936cf3aa
piaopiaosuo <863646686@qq.com>
2019-04-25 13:44:13 +0800

    test

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
-# Create your tests here.
 
+class HomePageTest(TestCase):
 
-class SmokeTest(TestCase):
-
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        print('123', found)
+        self.assertEqual(found.func, home_page)
